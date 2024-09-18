26bac4f492847963e8d408d73db0acbe98465a4f
Ivan Pakhomov <ip1133@ya.ru>
2024-09-18 23:11:17 +0300

    feat: complete multiple tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -303,9 +303,10 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.filter((e) => Math.abs(e) === e && Math.abs(e) !== 0).length;
 }
+
 
 /**
  * Sorts digit names
